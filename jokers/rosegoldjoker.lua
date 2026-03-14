04baf5e9a73d5f59bfc0080138b4f51a88c11634
GreenKookie56 <brandon05121@gmail.com>
2026-03-14 22:59:18 +0800

    rework

--- jokers/rosegoldjoker.lua
+++ jokers/rosegoldjoker.lua
@@ -3,14 +3,14 @@ SMODS.Joker{ --Rose Gold Joker
     key = "rosegoldjoker",
     config = {
         extra = {
-            slot_change = '1'
+            slot_change = '2'
         }
     },
     loc_txt = {
         ['name'] = 'Rose Gold Joker',
         ['text'] = {
-            [1] = '{C:dark_edition}+1{} Joker slot',
-            [2] = '{C:attention}+1{} Consumable slot'
+            [1] = '{C:attention}+2{} Consumable slots',
+            [2] = '{X:chips,C:white}X0.7{} Chips'
         },
         ['unlock'] = {
             [1] = 'Unlocked by default.'
@@ -34,39 +34,24 @@ SMODS.Joker{ --Rose Gold Joker
     atlas = 'CustomJokers',
     
     calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main and not context.blueprint then
+            return {
+                x_chips = 0.7
+            }
+        end
     end,
     
     add_to_deck = function(self, card, from_debuff)
-        G.jokers.config.card_limit = G.jokers.config.card_limit + 1
         G.E_MANAGER:add_event(Event({func = function()
-            G.consumeables.config.card_limit = G.consumeables.config.card_limit + 1
+            G.consumeables.config.card_limit = G.consumeables.config.card_limit + 2
             return true
         end }))
     end,
     
     remove_from_deck = function(self, card, from_debuff)
-        G.jokers.config.card_limit = G.jokers.config.card_limit - 1
         G.E_MANAGER:add_event(Event({func = function()
-            G.consumeables.config.card_limit = G.consumeables.config.card_limit - 1
+            G.consumeables.config.card_limit = G.consumeables.config.card_limit - 2
             return true
         end }))
     end
 }
-
-local check_for_buy_space_ref = G.FUNCS.check_for_buy_space
-G.FUNCS.check_for_buy_space = function(card)
-    if card.config.center.key == "j_sholextra_rosegoldjoker" then -- ignore slot limit when bought
-        return true
-    end
-    return check_for_buy_space_ref(card)
-end
-
-local can_select_card_ref = G.FUNCS.can_select_card
-G.FUNCS.can_select_card = function(e)
-    	if e.config.ref_table.config.center.key == "j_sholextra_rosegoldjoker" then
-        		e.config.colour = G.C.GREEN
-        		e.config.button = "use_card"
-    	else
-        		can_select_card_ref(e)
-    	end
-end
