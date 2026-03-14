fc297ed63bd1f98b6f2b08e8b6728e88069c9f7c
GreenKookie56 <brandon05121@gmail.com>
2026-03-14 13:12:35 +0800

    Add files via upload

--- jokers/nahuatljoker.lua
+++ jokers/nahuatljoker.lua
@@ -31,5 +31,10 @@ SMODS.Joker{ --Nahuatl Joker
     unlocked = true,
     discovered = true,
     atlas = 'Joker',
-    pools = { ["modprefix_sholiumx_jokers"] = true }
+    pools = { ["modprefix_sholiumx_jokers"] = true },
+    
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+        end
+    end
 }
