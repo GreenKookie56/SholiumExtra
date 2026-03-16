10724b9fa3633f594e922187b34c8d0063f4a70d
GreenKookie56 <brandon05121@gmail.com>
2026-03-16 20:47:18 +0800

    Revert "Add files via upload"
    This reverts commit f2b57a7ed990e1dbfd6e8bf2e1eb51cdde9fff91.

--- jokers/nanovirus.lua
+++ jokers/nanovirus.lua
@@ -1,25 +1,23 @@
-
 SMODS.Joker{ --Nano-virus
     key = "nanovirus",
     config = {
         extra = {
-            discount_amount = '1'
+            dollars = 2
         }
     },
     loc_txt = {
         ['name'] = 'Nano-virus',
         ['text'] = {
-            [1] = 'Everything in shop',
-            [2] = 'costs {C:money}$1{} less',
-            [3] = ''
+            [1] = 'Earn {C:gold}$2{}',
+            [2] = 'when a card is bought'
         },
         ['unlock'] = {
             [1] = 'Unlocked by default.'
         }
     },
     pos = {
-        x = 0,
-        y = 0
+        x = 1,
+        y = 3
     },
     display_size = {
         w = 71 * 1, 
@@ -28,48 +26,18 @@ SMODS.Joker{ --Nano-virus
     cost = 6,
     rarity = 1,
     blueprint_compat = true,
+    demicoloncompat = true,
     eternal_compat = true,
     perishable_compat = true,
     unlocked = true,
     discovered = true,
-    atlas = 'Joker',
-    
+    atlas = 'CustomJokers',
+
     calculate = function(self, card, context)
-    end,
-    
-    add_to_deck = function(self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                for k, v in pairs(G.I.CARD) do
-                if v.set_cost then v:set_cost() end
-                end
-                return true
-            end
-        }))
-    end,
-    
-    remove_from_deck = function(self, card, from_debuff)
-        G.E_MANAGER:add_event(Event({
-            func = function()
-                for k, v in pairs(G.I.CARD) do
-                if v.set_cost then v:set_cost() end
-                end
-                return true
-            end
-        }))
-    end
-} 
-      
-local card_set_cost_ref = Card.set_cost
-function Card:set_cost()
-    card_set_cost_ref(self)
-    
-    if next(SMODS.find_card("j_modprefix_nanovirus")) then
-        if (self.ability.set == 'Joker' or self.ability.set == 'Tarot' or self.ability.set == 'Planet' or self.ability.set == 'Spectral' or self.ability.set == 'Enhanced' or self.ability.set == 'Booster' or self.ability.set == 'Voucher') then
-            self.cost = math.max(0, self.cost - (1))
+        if context.buying_card or context.forcetrigger then
+                return {
+                    dollars = card.ability.extra.dollars
+                }
         end
     end
-    
-    self.sell_cost = math.max(1, math.floor(self.cost / 2)) + (self.ability.extra_value or 0)
-    self.sell_cost_label = self.facing == 'back' and '?' or self.sell_cost
-end
+}
