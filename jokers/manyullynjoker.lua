708dbf7283d3c375250e32e94b3cde6459c40b86
GreenKookie56 <brandon05121@gmail.com>
2026-03-27 20:23:58 +0800

    Update manyullynjoker.lua

--- jokers/manyullynjoker.lua
+++ jokers/manyullynjoker.lua
@@ -33,7 +33,7 @@ SMODS.Joker{ --Manyullyn Joker
     blueprint_compat = true,
     demicoloncompat = true,
     eternal_compat = true,
-    perishable_compat = true,
+    perishable_compat = false,
     unlocked = true,
     discovered = true,
     atlas = 'CustomJokers',
